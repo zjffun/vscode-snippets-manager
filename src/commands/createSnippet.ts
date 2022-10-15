39398751b9d902915d8814b56f1c0797334731d8
Jufeng Zhang <zjffun@gmail.com>
2022-10-15 11:29:52 +0800

    feat: select workspace folder
    Support select workspace folder when create snippets from untitled file.

--- src/commands/createSnippet.ts
+++ src/commands/createSnippet.ts
@@ -6,48 +6,91 @@ import refreshAllView from "../views/refreshAllView";
 
 const unsupportedScope = ["vue"];
 
+async function getSnippetUri() {
+  const { activeTextEditor } = vscode.window;
+
+  // use `default-snippets-manager.code-snippets` in WorkspaceFolder or UserFolder
+  const activeWorkspaceFolder =
+    activeTextEditor &&
+    vscode.workspace.getWorkspaceFolder(activeTextEditor.document.uri);
+
+  if (activeWorkspaceFolder?.uri) {
+    return vscode.Uri.joinPath(
+      activeWorkspaceFolder.uri,
+      ".vscode",
+      "default-snippets-manager.code-snippets"
+    );
+  }
+
+  if (vscode.workspace.workspaceFolders?.length === 1) {
+    return vscode.Uri.joinPath(
+      vscode.workspace.workspaceFolders[0].uri,
+      ".vscode",
+      "default-snippets-manager.code-snippets"
+    );
+  }
+
+  if (vscode.workspace.workspaceFolders?.length) {
+    const items = vscode.workspace.workspaceFolders.map((folder) => ({
+      label: folder.name,
+      uri: folder.uri,
+    }));
+
+    const workspaceFolder = await vscode.window.showQuickPick<{
+      label: string;
+      uri: vscode.Uri;
+    }>(items, {
+      placeHolder: "Select a workspace folder for the snippet",
+    });
+
+    if (!workspaceFolder) {
+      return;
+    }
+
+    return vscode.Uri.joinPath(
+      workspaceFolder.uri,
+      ".vscode",
+      "default-snippets-manager.code-snippets"
+    );
+  }
+
+  const askAddUserSnippets = context.globalState.get(
+    "askAddUserSnippets",
+    true
+  );
+
+  if (askAddUserSnippets) {
+    const answer = await vscode.window.showInformationMessage(
+      "Can't find workspace folder, do you want to add the snippet to user snippets?",
+      ...["Yes, and don't ask me again", "Yes", "No"]
+    );
+
+    if (answer === "No") {
+      return;
+    }
+
+    if (answer === "Yes, and don't ask me again") {
+      context.globalState.update("askAddUserSnippets", false);
+    }
+
+    return vscode.Uri.joinPath(
+      getUserFolderUri(),
+      "default-snippets-manager.code-snippets"
+    );
+  }
+}
+
 export default async (prefix?: string, snippetUri?: vscode.Uri) => {
   const { activeTextEditor } = vscode.window;
 
   let _snippetUri = snippetUri;
 
   if (!_snippetUri) {
-    // use `default-snippets-manager.code-snippets` in WorkspaceFolder or UserFolder
-    const activeWorkspaceFolder =
-      activeTextEditor &&
-      vscode.workspace.getWorkspaceFolder(activeTextEditor.document.uri);
+    _snippetUri = await getSnippetUri();
+  }
 
-    if (!activeWorkspaceFolder?.uri) {
-      const askAddUserSnippets = context.globalState.get(
-        "askAddUserSnippets",
-        true
-      );
-      if (askAddUserSnippets) {
-        const answer = await vscode.window.showInformationMessage(
-          "Can't find `activeWorkspaceFolder`, do you want to add the snippet to user snippets?",
-          ...["Yes, and don't ask me again", "Yes", "No"]
-        );
-
-        if (answer === "No") {
-          return;
-        }
-
-        if (answer === "Yes, and don't ask me again") {
-          context.globalState.update("askAddUserSnippets", false);
-        }
-      }
-
-      _snippetUri = vscode.Uri.joinPath(
-        getUserFolderUri(),
-        "default-snippets-manager.code-snippets"
-      );
-    } else {
-      _snippetUri = vscode.Uri.joinPath(
-        activeWorkspaceFolder?.uri,
-        ".vscode",
-        "default-snippets-manager.code-snippets"
-      );
-    }
+  if (!_snippetUri) {
+    return;
   }
 
   let _prefix = prefix;
