0edbae6a05b3ec0959b87505b0edc25e7eb74609
Jufeng Zhang <zjffun@gmail.com>
2024-07-08 00:40:50 +0800

    chore: test

---
+++ src/test/runTestWeb.ts
@@ -0,0 +1,30 @@
+import * as path from "path";
+import { runTests } from "@vscode/test-web";
+
+async function main() {
+  try {
+    // The folder containing the Extension Manifest package.json
+    // Passed to `--extensionDevelopmentPath`
+    const extensionDevelopmentPath = path.resolve(__dirname, "../../");
+
+    // The path to test runner
+    // Passed to --extensionTestsPath
+    const extensionTestsPath = path.resolve(
+      __dirname,
+      "../../dist/web/test/suite/index"
+    );
+
+    // Download VS Code, unzip it and run the integration test
+    await runTests({
+      browserType: "chromium",
+      extensionDevelopmentPath,
+      extensionTestsPath,
+      folderPath: path.join(extensionDevelopmentPath, "test.temp"),
+    });
+  } catch (err) {
+    console.error("Failed to run tests");
+    process.exit(1);
+  }
+}
+
+main();
