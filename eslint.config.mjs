c37c4502c71a08707a2b313c8e16f7cd4007cefb
Jufeng Zhang <zjffun@gmail.com>
2024-08-24 16:44:55 +0800

    feat: editorconfig, eslint and prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,32 @@
+import { includeIgnoreFile } from "@eslint/compat";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import eslint from "@eslint/js";
+import prettierConfig from "eslint-config-prettier";
+import tseslint from "typescript-eslint";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const gitignorePath = path.resolve(__dirname, ".gitignore");
+
+export default tseslint.config(
+  includeIgnoreFile(gitignorePath),
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  prettierConfig,
+  {
+    files: ['**/*.{ts,tsx,js}'],
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-this-alias": "off",
+      "no-undef": "off",
+    },
+  },
+  {
+    files: ['**/*.js'],
+    rules: {
+      "@typescript-eslint/no-require-imports": "off",
+    },
+  },
+);
