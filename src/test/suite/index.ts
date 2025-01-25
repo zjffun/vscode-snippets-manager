6db8b67f555c0e435faccf3fe4d09d1ae2436de2
Jufeng Zhang <zjffun@gmail.com>
2025-01-25 22:37:18 +0800

    fix: require mocha

--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -1,6 +1,8 @@
 import * as path from "path";
-import * as Mocha from "mocha";
 import * as glob from "glob";
+
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const Mocha = require("mocha");
 
 export function run(): Promise<void> {
   // Create the mocha test
@@ -23,7 +25,7 @@ export function run(): Promise<void> {
 
       try {
         // Run the mocha test
-        mocha.run((failures) => {
+        mocha.run((failures: any) => {
           if (failures > 0) {
             e(new Error(`${failures} tests failed.`));
           } else {
